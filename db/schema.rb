e63b6efb471f864ed9e60ee58d129f98fb73efb0
yosnak13 <yosnak13@gmail.com>
2020-05-30 13:26:42 +0900

    Word limit is given

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200528065319) do
+ActiveRecord::Schema.define(version: 20200530040105) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
